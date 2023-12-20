872bb9d448e006740bf51a7ffaa0a30423ac47e6
Sultan Lenvu <slemvu@mail.ru>
2023-12-20 12:59:53 +0300

    working app

---
+++ achievement2/postgres-container-run.sh
@@ -0,0 +1,1 @@
+docker run -d -p 5432:5432 --name postgres-container -e POSTGRES_DB=testdb -e POSTGRES_USER=sultan -e POSTGRES_PASSWORD=lenvu postgres
--- achievement2/sources/app.py
+++ achievement2/sources/app.py
@@ -1,127 +1,77 @@
-<<<<<<< Updated upstream:achievement2/sources/app.py
 from flask import Flask, request, jsonify
+import os
 import psycopg2
 
 app = Flask(__name__)
 
-# Подключение к базе данных PostgreSQL
-# conn = psycopg2.connect(dbname='studydb', user='postgres', password='1995', host='localhost', port='5432')
+# Подключение к базе данных Postgres через переменные окружения
+db_host = os.environ.get('DB_HOST')
+db_port = os.environ.get('DB_PORT')
+db_name = os.environ.get('DB_NAME')
+db_user = os.environ.get('DB_USER')
+db_password = os.environ.get('DB_PASSWORD')
 
-@app.route('/')
-def hello_world():
-    return "Hello world!"
+# db_host = 'postgres-container'
+# db_port = '5432'
+# db_name = 'testdb'
+# db_user = 'sultan'
+# db_password = 'lenvu'
 
-# @app.route('/', methods=['POST'])
-# def process_number():
-#     number = int(request.form.get('number'))
-#
-#     try:
-#         # Проверка, если число уже обрабатывалось
-#         if is_number_processed(number):
-#             return jsonify({'error': 'Number has already been processed.'}), 400
-#
-#         # Проверка, если полученное число на единицу меньше уже обработанного числа
-#         if is_number_less_than_previous(number):
-#             return jsonify({'error': 'Number is less than the previous processed number.'}), 400
-#
-#         # Увеличение числа на единицу
-#         processed_number = number + 1
-#
-#         # Сохранение числа в базе данных
-#         save_number(processed_number)
-#
-#         return jsonify({'processed_number': processed_number})
-#
-#     except Exception as e:
-#         # Обработка исключительных ситуаций
-#         return jsonify({'error': str(e)}), 500
-#
-# def is_number_processed(number):
-#     # Проверка наличия числа в базе данных
-#     cur = conn.cursor()
-#     cur.execute("SELECT * FROM numbers WHERE value = %s", (number,))
-#     result = cur.fetchone()
-#     cur.close()
-#     return result is not None
-#
-# def is_number_less_than_previous(number):
-#     # Получение последнего обработанного числа из базы данных
-#     cur = conn.cursor()
-#     cur.execute("SELECT MAX(value) FROM numbers")
-#     previous_number = cur.fetchone()[0]
-#     cur.close()
-#     return previous_number is not None and number < previous_number
-#
-# def save_number(number):
-#     # Сохранение числа в базе данных
-#     cur = conn.cursor()
-#     cur.execute("INSERT INTO numbers (value) VALUES (%s)", (number,))
-#     conn.commit()
-#     cur.close()
+# Создание таблицы при запуске приложения
+conn = psycopg2.connect(
+        host=db_host,
+        port=db_port,
+        dbname=db_name,
+        user=db_user,
+        password=db_password
+)
+cur = conn.cursor()
+cur.execute('CREATE TABLE IF NOT EXISTS numbers (value INTEGER PRIMARY KEY)')
+conn.commit()
+cur.close()
+conn.close()
 
-if __name__ == '__main__':
-    app.run(debug=True, host = '0.0.0.0', port=5000)
-=======
-from flask import Flask, request, jsonify
-import psycopg2
-
-app = Flask(__name__)
-
-# Подключение к базе данных PostgreSQL
-# conn = psycopg2.connect(dbname='studydb', user='postgres', password='1995', host='localhost', port='5432')
-
-@app.route('/')
-def hello_world():
-    return "Hello world!"
 
 @app.route('/', methods=['POST'])
 def process_number():
-    number = int(request.form.get('number'))
+    number = request.json.get('number')
 
-    try:
-        # Проверка, если число уже обрабатывалось
-        if is_number_processed(number):
-            return jsonify({'error': 'Number has already been processed.'}), 400
-
-        # Проверка, если полученное число на единицу меньше уже обработанного числа
-        if is_number_less_than_previous(number):
-            return jsonify({'error': 'Number is less than the previous processed number.'}), 400
-
-        # Увеличение числа на единицу
-        processed_number = number + 1
-
-        # Сохранение числа в базе данных
-        save_number(processed_number)
-
-        return jsonify({'processed_number': processed_number})
-
-    except Exception as e:
-        # Обработка исключительных ситуаций
-        return jsonify({'error': str(e)}), 500
-
-def is_number_processed(number):
-    # Проверка наличия числа в базе данных
+    # Проверка, что число не было обработано ранее
+    conn = psycopg2.connect(
+            host=db_host,
+            port=db_port,
+            dbname=db_name,
+            user=db_user,
+            password=db_password
+    )
     cur = conn.cursor()
-    cur.execute("SELECT * FROM numbers WHERE value = %s", (number,))
-    result = cur.fetchone()
-    cur.close()
-    return result is not None
+    cur.execute('SELECT value FROM numbers WHERE value = %s', (number,))
+    existing_number = cur.fetchone()
+    if existing_number:
+        cur.close()
+        conn.close()
+        error_message = 'Число уже обработано'
+        app.logger.error(error_message)
+        return jsonify({'error': error_message}), 400
 
-def is_number_less_than_previous(number):
-    # Получение последнего обработанного числа из базы данных
-    cur = conn.cursor()
-    cur.execute("SELECT MAX(value) FROM numbers")
-    previous_number = cur.fetchone()[0]
-    cur.close()
-    return previous_number is not None and number < previous_number
+    # Проверка, что число на единицу больше предыдущего обработанного числа
+    cur.execute('SELECT value FROM numbers ORDER BY value DESC LIMIT 1')
+    previous_number = cur.fetchone()
+    if previous_number and number != previous_number[0] + 1:
+        cur.close()
+        conn.close()
+        error_message = 'Нарушена последовательность чисел'
+        app.logger.error(error_message)
+        return jsonify({'error': error_message}), 400
 
-def save_number(number):
     # Сохранение числа в базе данных
-    cur = conn.cursor()
-    cur.execute("INSERT INTO numbers (value) VALUES (%s)", (number,))
+    cur.execute('INSERT INTO numbers (value) VALUES (%s)', (number,))
     conn.commit()
     cur.close()
+    conn.close()
+
+    return jsonify({'result': number + 1})
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host = '0.0.0.0', port=5000)
->>>>>>> Stashed changes:sources/app.py
+    app.run(host='0.0.0.0')
