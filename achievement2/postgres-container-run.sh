7259d33b85fff1c95e99e4ee2990a51608b75593
sultan <slenvu@mail.ru>
2023-12-20 14:10:07 +0300

    yamls ready

--- achievement2/postgres-container-run.sh
+++ achievement2/postgres-container-run.sh
@@ -1,1 +1,8 @@
 docker run -d -p 5432:5432 --name postgres-container -e POSTGRES_DB=testdb -e POSTGRES_USER=sultan -e POSTGRES_PASSWORD=lenvu postgres
+
+docker run -d -p 5000:5000 --name app-container --link postgres-container sultanlenvu/node13_pyapp
+
+curl -X POST name-13.hse.ru:30080/ -H "Content-Type: application/json" -d '{ "number": 1 }'
+
+curl -X POST -H "Content-Type: application/json" -d '{"number": 1}' name-19.hse.ru:30080
+
